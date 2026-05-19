5e230739ae9976d2223e4199da00ca63c7af2de8
omar salas <oemsalas@gmail.com>
2026-05-19 15:54:00 -0300

    fix: corregir build.gradle.kts para GitHub Actions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
@@ -26,8 +26,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = flutter.versionCode()
+	versionName = flutter.versionName()
     }
 
     buildTypes {
